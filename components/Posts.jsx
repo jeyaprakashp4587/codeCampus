2299386253b2aa5f444e731c791779c6658b530f
jeyaprakashp4587 <jeyaprakashp022@gmail.com>
2024-09-12 10:09:38 +0530

    post update

--- components/Posts.jsx
+++ components/Posts.jsx
@@ -4,17 +4,11 @@ import {
   Text,
   View,
   TouchableOpacity,
-  ScrollView,
   FlatList,
 } from "react-native";
 import React, { useState } from "react";
 import { Colors, font } from "../constants/Colors";
-import {
-  faComment,
-  faCommentDots,
-  faComments,
-  faHeart,
-} from "@fortawesome/free-regular-svg-icons";
+import { faComments, faHeart } from "@fortawesome/free-regular-svg-icons";
 import { Dimensions } from "react-native";
 import Ripple from "react-native-material-ripple";
 import Api from "../Api";
@@ -25,26 +19,29 @@ import RelativeTime from "./RelativeTime";
 
 const Posts = ({ post, index, admin, updateLikeCount }) => {
   const { width, height } = Dimensions.get("window");
-  let initialText = post.item?.PostText ? post.item?.PostText : post.PostText;
+  const initialText = post.item?.PostText || post.PostText;
   const { user, setUser } = useData();
   const wordThreshold = 20;
   const [expanded, setExpanded] = useState(false);
+
   const toggleExpanded = () => {
     setExpanded(!expanded);
   };
+
   // Function to count words in a string
-  const countWords = (text) => {
-    return text?.trim().split(/\s+/).length;
-  };
+  const countWords = (text) => text?.trim().split(/\s+/).length;
+
   // delete disp state
   const [deldisplay, setDeldisplay] = useState(false);
+
   const handleDelDisp = () => {
     setDeldisplay((prev) => !prev);
   };
+
   // handle Delete
   const HandleDelete = async (postId) => {
     try {
-      const res = await axios.post.item(`${Api}/Post/deletePost/${user._id}`, {
+      const res = await axios.post(`${Api}/Post/deletePost/${user._id}`, {
         postId: postId,
       });
       if (res.data) {
@@ -54,34 +51,29 @@ const Posts = ({ post, index, admin, updateLikeCount }) => {
       console.log(err);
     }
   };
+
   // like
-  const [likeCount, setLikeCount] = useState(
-    post?.item?.Like ? post.item.Like : post?.Like
-  );
+  const [likeCount, setLikeCount] = useState(post?.item?.Like || post?.Like);
+
   const handleLike = async () => {
     try {
-      // Send request to the backend to increment the like count
       const res = await axios.post(
-        `${Api}/Post/likePost/${post.item._id ? post.item._id : post._id}`
+        `${Api}/Post/likePost/${post.item?._id || post._id}`
       );
       if (res.status === 200) {
-        // Update the like count locally
         setLikeCount(likeCount + 1);
-        updateLikeCount(
-          post?.item?.Like ? post.item?.Like : post?.Like,
-          likeCount + 1
-        ); // Update the like count in parent component
+        updateLikeCount(post?.item?.Like || post?.Like, likeCount + 1);
       }
     } catch (error) {
-      console.error("Error liking the post.item", error);
+      console.error("Error liking the post", error);
     }
   };
+
   return (
     <View
       key={index}
       style={{
-        // borderWidth: 1,
-        height: expanded ? "auto" : "auto",
+        height: "auto",
         marginTop: 20,
         padding: 20,
         borderRadius: 10,
@@ -90,10 +82,9 @@ const Posts = ({ post, index, admin, updateLikeCount }) => {
         marginBottom: 10,
         marginHorizontal: 5,
         position: "relative",
-        // margin: 10,
       }}
     >
-      {/* post.item header */}
+      {/* Post Header */}
       <View
         style={{
           flexDirection: "row",
@@ -115,7 +106,7 @@ const Posts = ({ post, index, admin, updateLikeCount }) => {
             }}
           >
             {post.item?.firstName
-              ? post.item?.firstName + post.item?.LastName
+              ? post.item.firstName + post.item.LastName
               : post.firstName + post.LastName}
           </Text>
           <Text
@@ -125,45 +116,46 @@ const Posts = ({ post, index, admin, updateLikeCount }) => {
               fontFamily: font.poppins,
             }}
           >
-            {post.item?.Institute ? post.item?.Institute : post.Institute}
+            {post.item?.Institute || post.Institute}
           </Text>
         </View>
         {admin && (
-          <TouchableOpacity onPress={() => handleDelDisp()}>
+          <TouchableOpacity onPress={handleDelDisp}>
             <Image
               source={{ uri: "https://i.ibb.co/nn25gZN/menu.png" }}
               style={{ width: 20, height: 20, tintColor: Colors.lightGrey }}
             />
           </TouchableOpacity>
         )}
-        {/* delete section */}
-        <TouchableOpacity
-          onPress={() =>
-            HandleDelete(post.item?._id ? post.item?._id : post._id)
-          }
-          style={{
-            position: "absolute",
-            right: width * 0.03,
-            top: height * 0.07,
-            backgroundColor: "orange",
-            borderRadius: 4,
-            display: deldisplay ? "flex" : "none",
-            zIndex: 100,
-          }}
-        >
-          <Text
+
+        {/* Delete Section */}
+        {deldisplay && (
+          <TouchableOpacity
+            onPress={() => HandleDelete(post.item?._id || post._id)}
             style={{
-              color: "white",
-              letterSpacing: 1,
-              paddingHorizontal: 15,
-              paddingVertical: 5,
+              position: "absolute",
+              right: width * 0.03,
+              top: height * 0.07,
+              backgroundColor: "orange",
+              borderRadius: 4,
+              zIndex: 100,
             }}
           >
-            Delete
-          </Text>
-        </TouchableOpacity>
+            <Text
+              style={{
+                color: "white",
+                letterSpacing: 1,
+                paddingHorizontal: 15,
+                paddingVertical: 5,
+              }}
+            >
+              Delete
+            </Text>
+          </TouchableOpacity>
+        )}
       </View>
-      {/* post.item about */}
+
+      {/* Post Text */}
       <Text
         style={{
           fontFamily: font.poppins,
@@ -174,7 +166,7 @@ const Posts = ({ post, index, admin, updateLikeCount }) => {
       >
         {expanded
           ? initialText
-          : `${initialText?.split(" ").slice(0, wordThreshold).join(" ")}...`}
+          : `${initialText.split(" ").slice(0, wordThreshold).join(" ")}...`}
       </Text>
       {countWords(initialText) > wordThreshold && (
         <TouchableOpacity onPress={toggleExpanded} style={styles.showMore}>
@@ -183,36 +175,35 @@ const Posts = ({ post, index, admin, updateLikeCount }) => {
           </Text>
         </TouchableOpacity>
       )}
-      <Text style={{ color: Colors.violet }}>{post.item?.PostLink}</Text>
-      {/* post.item image */}
-      {post.item?.Images
-        ? post.item?.Images
-        : post.Images && (
-            <FlatList
-              data={post.item?.Images ? post.item?.Images : post.Images}
-              style={{ borderWidth: 0 }}
-              horizontal={true}
-              renderItem={({ item, index }) => (
-                <Image
-                  key={index}
-                  source={{ uri: item }}
-                  style={{
-                    width:
-                      post.item?.Images.length == 1
-                        ? width * 0.84
-                        : width * 0.8,
-                    height: height * 0.3,
-                    objectFit: "contain",
-                    borderWidth: 1,
-                  }}
-                />
-              )}
-            />
-          )}
-      {/* post.item sections */}
+      <Text style={{ color: Colors.violet }}>
+        {post.item?.PostLink ? post.item?.PostLink : post?.PostLink}
+      </Text>
+
+      {/* Post Image */}
+      {post.item?.Images ||
+        (post?.Images && (
+          <FlatList
+            data={post?.item?.Images ? post.item?.Images : post?.Images}
+            horizontal
+            renderItem={({ item, index }) => (
+              <Image
+                key={index}
+                source={{ uri: item }}
+                style={{
+                  width:
+                    post.item?.Images.length === 1 ? width * 0.84 : width * 0.8,
+                  height: height * 0.3,
+                  resizeMode: "contain",
+                  borderWidth: 1,
+                }}
+              />
+            )}
+          />
+        ))}
+
+      {/* Post Footer */}
       <View
         style={{
-          // borderWidth: 1,
           flexDirection: "row",
           justifyContent: "space-around",
           marginTop: 5,
@@ -221,18 +212,14 @@ const Posts = ({ post, index, admin, updateLikeCount }) => {
       >
         <TouchableOpacity
           onPress={handleLike}
-          style={{ flexDirection: "row", alignItems: "center", columnGap: 10 }}
+          style={{ flexDirection: "row", alignItems: "center" }}
         >
           <Text style={{ fontFamily: font.poppins, fontSize: width * 0.04 }}>
-            {post.item?.Like ? post.item?.Like : post.Like}
+            {likeCount}
           </Text>
           <FontAwesomeIcon size={20} icon={faHeart} color={Colors.mildGrey} />
-          {/* <Image
-            source={require("../assets/images/like.png")}
-            style={{ width: 25, height: 25, tintColor: Colors.lightGrey }}
-          /> */}
         </TouchableOpacity>
-        <TouchableOpacity style={{ flexDirection: "row", columnGap: 10 }}>
+        <TouchableOpacity style={{ flexDirection: "row" }}>
           <Text style={{ fontFamily: font.poppins, fontSize: width * 0.04 }}>
             12
           </Text>
@@ -244,7 +231,7 @@ const Posts = ({ post, index, admin, updateLikeCount }) => {
         </TouchableOpacity>
 
         <RelativeTime
-          time={post.item?.Time ? post.item?.Time : post.Time}
+          time={post.item?.Time || post.Time}
           fsize={width * 0.033}
         />
       </View>
@@ -254,4 +241,8 @@ const Posts = ({ post, index, admin, updateLikeCount }) => {
 
 export default Posts;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  showMore: {
+    marginTop: 5,
+  },
+});
